2d5f0da76432f055fe61c4620c218b0b3a9df3fb
adelmodias <adelmodias@outlook.com>
2020-02-12 21:30:27 -0300

    Working in ads page and left sidebar

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { PageArea } from './styled';
 import useApi from '../../helpers/OlxAPI';
 
 import { PageContainer } from '../../components/MainComponents';
-import AdItem from '../../components/partials/adItem';
+// import AdItem from '../../components/partials/adItem';
 
 const Ads = () => {
     const api = useApi();
+
+    const useQueryString = () => {
+        return new URLSearchParams( useLocation().search );
+    }
+    const query = useQueryString();
+
+    const [q, setQ] = useState( query.get('q') != null ? query.get('q') : '' );
+    const [cat, setCat] = useState( query.get('cat') != null ? query.get('cat') : '' );
+    const [state, setState] = useState( query.get('state') != null ? query.get('state') : '' );
 
     const [stateList, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -45,24 +54,24 @@ const Ads = () => {
             <PageArea>
                 <div className="leftSide">
                     <form method="GET">
-                        <input type="text" name="q" placeholder="O que você procura?" />
+                        <input type="text" name="q" placeholder="O que você procura?" value={q} />
                         
                         <div className="filterName">Estado:</div>
-                        <select name="state">
+                        <select name="state" value={state}>
                             {stateList.map((item, index) => <option key={index} value={item.name}>{item.name}</option>)}
                         </select>
 
                         <div className="filterName">Categoria:</div>
                         <ul>
                             {categories.map( (item, index) => 
-                                <li key={index} value={item.name} className="categoryItem">
+                                <li key={index} value={item.name} className={cat==item.slug?'categoryItem active':'categoryItem'}>
                                     <img src={item.img} alt={item.name} />
                                     <span>{item.name}</span>
                                 </li>
                             ) }
                         </ul>
 
-                        <button>Pesquisa</button>
+                        {/* <button>Pesquisa</button> */}
                     </form>
                 </div>
 
