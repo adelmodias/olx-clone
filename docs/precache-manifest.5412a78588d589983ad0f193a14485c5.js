9395e4bc76291ac7146337eb385397f1b9556c15
adelmodias <adelmodias@outlook.com>
2020-02-15 22:05:05 -0300

    Deploy first version

---
+++ docs/precache-manifest.5412a78588d589983ad0f193a14485c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f52c133e5281c1aa3722b578336eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f24b68f417140ce1b9e",
+    "url": "/static/css/2.3e14f9b5.chunk.css"
+  },
+  {
+    "revision": "ed9d282b3e71f4f3cd31",
+    "url": "/static/css/main.cb6c6e55.chunk.css"
+  },
+  {
+    "revision": "4f24b68f417140ce1b9e",
+    "url": "/static/js/2.e9d86db3.chunk.js"
+  },
+  {
+    "revision": "1c7168f59c5db841e6d613c714f0e5d8",
+    "url": "/static/js/2.e9d86db3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed9d282b3e71f4f3cd31",
+    "url": "/static/js/main.5a44d88c.chunk.js"
+  },
+  {
+    "revision": "8a033b272dab71f5fd6c",
+    "url": "/static/js/runtime-main.9552c41e.js"
+  }
+]);
