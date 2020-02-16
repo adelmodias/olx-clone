2a398ab7b4b1734db8d3f4aaf1ec136fd6930c6b
adelmodias <adelmodias@outlook.com>
2020-02-15 22:42:44 -0300

    Revert "Pagination working"
    This reverts commit d9121c931c8f1f0afd1b1b17c14190983e09edea.

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -32,15 +32,12 @@ const Ads = () => {
     const [loading, setLoading] = useState(true);
 
     const getAdsList = async () => {
-        var offset = (currentPage - 1) * 2;
-
         const json = await api.getAds({
             sort: 'desc',
-            limit: 6,
+            limit: 9,
             q,
             cat,
-            state,
-            offset
+            state
         });
         setAdsList(json.ads);
         setTotalAds(json.total);
@@ -57,11 +54,6 @@ const Ads = () => {
         }
 
     }, [totalAds]);
-
-    useEffect(() => {
-        setResultOpacity(0.3);
-        getAdsList();
-    }, [currentPage]);
 
     useEffect(() => {
         let queryString = [];
@@ -158,7 +150,7 @@ const Ads = () => {
                 <div className="rightSide">
                     <h2>Resultados da busca</h2>
 
-                    {loading && adsList.length === 0 &&
+                    {loading && 
                         <div className="listWarning">Carregando...</div>
                     }
                     {!loading && adsList.length === 0 &&
