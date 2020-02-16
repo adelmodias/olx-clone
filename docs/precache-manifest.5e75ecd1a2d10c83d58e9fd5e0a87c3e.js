817a47837f860467d522fc7509e6e47d4ba63752
adelmodias <adelmodias@outlook.com>
2020-02-15 22:31:58 -0300

    Deploy first version

---
+++ docs/precache-manifest.5e75ecd1a2d10c83d58e9fd5e0a87c3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d3ad8d73f780bc88d56fe0f549020c",
+    "url": "/olx-clone/index.html"
+  },
+  {
+    "revision": "888253e7c32c8bc6154d",
+    "url": "/olx-clone/static/css/2.3e14f9b5.chunk.css"
+  },
+  {
+    "revision": "51fb868febc012cd0cf3",
+    "url": "/olx-clone/static/css/main.cb6c6e55.chunk.css"
+  },
+  {
+    "revision": "888253e7c32c8bc6154d",
+    "url": "/olx-clone/static/js/2.3d153495.chunk.js"
+  },
+  {
+    "revision": "1c7168f59c5db841e6d613c714f0e5d8",
+    "url": "/olx-clone/static/js/2.3d153495.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51fb868febc012cd0cf3",
+    "url": "/olx-clone/static/js/main.9444e435.chunk.js"
+  },
+  {
+    "revision": "d77d8b415e2229f6a85a",
+    "url": "/olx-clone/static/js/runtime-main.4fd6d087.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7e4496e2f5b6472ed7134aed8a11e8a1.js"
+  "/olx-clone/precache-manifest.5e75ecd1a2d10c83d58e9fd5e0a87c3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/olx-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
